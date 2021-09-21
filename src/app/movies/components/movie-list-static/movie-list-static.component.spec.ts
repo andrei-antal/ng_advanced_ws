7955ae45773e31146c29aaa4b763726d2fa30215
Andrei Antal <antal.andrei@icloud.com>
2021-09-21 12:13:55 +0300

    Done challenge 16

--- src/app/movies/components/movie-list-static/movie-list-static.component.spec.ts
+++ src/app/movies/components/movie-list-static/movie-list-static.component.spec.ts
@@ -2,6 +2,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { MovieListStaticComponent } from './movie-list-static.component';
+import { MovieStaticService } from '../../services/movie-static.service';
+
+const mockMovieService = {
+  movies: [{ comment: '' }, { comment: '' }, { comment: '' }],
+};
 
 describe('MovieListStaticComponent', () => {
   let component: MovieListStaticComponent;
@@ -10,6 +15,7 @@ describe('MovieListStaticComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [MovieListStaticComponent],
+      providers: [{ provide: MovieStaticService, useValue: mockMovieService }],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   });
@@ -22,5 +28,14 @@ describe('MovieListStaticComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should correctly render the movies list', () => {
+    // Arrange + Act
+    const moviesList: HTMLElement[] =
+      fixture.nativeElement.querySelectorAll('ngi-movie-item');
+
+    // Assert
+    expect(moviesList.length).toBe(mockMovieService.movies.length);
   });
 });
