6eae76842e512be1273127e97047a8165713820c
Andrei Antal <antal.andrei@icloud.com>
2021-09-21 11:04:12 +0300

    Done challenge 12

--- src/app/movies/services/movies-validators.service.spec.ts
+++ src/app/movies/services/movies-validators.service.spec.ts
@@ -1,7 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-import { MovieGenreAsyncValidator } from './movies-validators.service';
+import {
+  genreValidator,
+  MovieGenreAsyncValidator,
+  sciFiGenreYearValidator,
+} from './movies-validators.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 describe('MovieGenreAsyncValidator', () => {
   let service: MovieGenreAsyncValidator;
@@ -15,5 +20,52 @@ describe('MovieGenreAsyncValidator', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+});
+
+describe('genreValidator', () => {
+  it('should validate genre', () => {
+    // Arrange
+    const fc = new FormControl();
+    fc.setValidators(genreValidator);
+
+    // Act
+    fc.setValue(['action', 'thriller']);
+    // Assert
+    expect(fc.status).toBe('VALID');
+
+    // Act
+    fc.setValue(['comedy']);
+    // Assert
+    expect(fc.status).toBe('VALID');
+
+    // Act
+    fc.setValue(['action', 'thrlr']);
+    // Assert
+    expect(fc.status).toBe('INVALID');
+
+    // Act
+    fc.setValue(['cmdy']);
+    // Assert
+    expect(fc.status).toBe('INVALID');
+  });
+});
+
+describe('sciFiGenreYearValidator', () => {
+  it('', () => {
+    const genreCtrl = new FormControl('sci-fi');
+    const yearCtrl = new FormControl();
+    const fg = new FormGroup({
+      genre: genreCtrl,
+      year: yearCtrl,
+    });
+
+    fg.setValidators(sciFiGenreYearValidator);
+
+    yearCtrl.setValue('2000');
+    expect(fg.status).toBe('VALID');
+
+    yearCtrl.setValue('1900');
+    expect(fg.status).toBe('INVALID');
   });
 });
