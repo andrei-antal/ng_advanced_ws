128b4376377db36edd5351d18ad2e248cefc6217
Andrei Antal <antal.andrei@icloud.com>
2021-09-22 23:19:19 +0300

    Done challenge 19

---
+++ src/app/movies/store/movies.actions.ts
@@ -0,0 +1,14 @@
+import { createAction, props } from '@ngrx/store';
+import { Movie } from '../model/movie';
+
+export const loadMovies = createAction('[Movies] Load movies');
+
+export const loadMoviesSuccess = createAction(
+  '[Movies] Load movies success',
+  props<{ movies: Movie[] }>()
+);
+
+export const loadMoviesFail = createAction(
+  '[Movies] Load movies fail',
+  props<any>()
+);
