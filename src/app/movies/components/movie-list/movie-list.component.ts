01906c033769386dfeacf77be566f8d3d6fc4b53
Andrei Antal <antal.andrei@icloud.com>
2021-09-23 22:53:55 +0300

    Done challenge 22

--- src/app/movies/components/movie-list/movie-list.component.ts
+++ src/app/movies/components/movie-list/movie-list.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { debounceTime, startWith, switchMap } from 'rxjs/operators';
+import { Store, select } from '@ngrx/store';
+
+import { debounceTime, startWith, switchMap, tap } from 'rxjs/operators';
 
 import { Movie } from '../../model/movie';
 import { CommentUpdate } from '../movie-item/movie-item.component';
 import { MovieService } from '../../services/movie.service';
+import { MovieState, getAllMovies } from '../../store/movies.reducer';
 @Component({
   selector: 'ngi-movie-list',
   templateUrl: './movie-list.component.html',
@@ -15,24 +18,30 @@ export class MovieListComponent implements OnInit {
   public movies$: Observable<Movie[]>;
   public searchField = new FormControl('');
 
-  constructor(public movieService: MovieService) {}
+  constructor(private store: Store<MovieState>) {}
 
   public ngOnInit(): void {
-    this.movies$ = this.searchField.valueChanges.pipe(
-      debounceTime(300),
-      startWith(undefined),
-      switchMap((searchTerm) => this.movieService.getMovies(searchTerm))
+    this.movies$ = this.store.pipe(
+      select(getAllMovies),
+      tap((state) => {
+        console.log(state);
+      })
     );
+    // this.movies$ = this.searchField.valueChanges.pipe(
+    //   debounceTime(300),
+    //   startWith(undefined),
+    //   switchMap((searchTerm) => this.movieService.getMovies(searchTerm))
+    // );
   }
 
   public handleCommentUpdate(commentPayload: CommentUpdate): void {
-    this.movieService
-      .updateComment(commentPayload.id, commentPayload.newComment)
-      .subscribe();
+    // this.movieService
+    //   .updateComment(commentPayload.id, commentPayload.newComment)
+    //   .subscribe();
   }
 
   public handleMovieDelete(movieId: string): void {
-    this.movieService.deleteMovie(movieId).subscribe();
+    // this.movieService.deleteMovie(movieId).subscribe();
   }
 
   public trackByFn(_: any, movie: Movie): string {
--- src/app/movies/store/movies.reducer.ts
+++ src/app/movies/store/movies.reducer.ts
@@ -11,7 +11,18 @@ export interface MovieState {
 }
 
 const initialState: MovieState = {
-  movies: [],
+  movies: [
+    {
+      id: '1',
+      title: 'Star Wars: The Last Jedi',
+      year: '2017',
+      genre: 'Action, Adventure, Fantasy',
+      plot: 'Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.',
+      poster:
+        'https://images-na.ssl-images-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg',
+      comment: '',
+    },
+  ],
   loading: false,
   error: undefined,
 };
